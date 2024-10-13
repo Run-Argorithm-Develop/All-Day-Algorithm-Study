0c1fcf9554d42d005cb3ef02572c9fdac1800938
HaRim <76032947+HwangHarim@users.noreply.github.com>
2024-10-14 06:00:48 +0900

    add : 10_14(슬라이딩 윈도우)

---
+++ 하림/oct_14/SlidingWindowMaximum_239_leetcode.java
@@ -0,0 +1,25 @@
+package 하림.oct_14;
+
+import java.util.*;
+
+public class SlidingWindowMaximum_239_leetcode {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i =0; i<nums.length;i++){
+            if(!dq.isEmpty()&& dq.peek()< i-k+1){
+                dq.poll();
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+                dq.pollLast();
+            }
+            dq.add(i);
+            if(i >= k-1){
+                result.add(nums[dq.peek()]);
+            }
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
